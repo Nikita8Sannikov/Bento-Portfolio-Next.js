40e5ccd8020be3a1319f8d6384e7c3d7c6aa9b2d
Nikita8Sannikov <Ak-4t7@mail.ru>
2026-07-28 20:43:03 +0600

    refactor: refactor types

--- src/types/bento.ts
+++ src/types/bento.ts
@@ -1,10 +1,40 @@
 export type TileSize = "square" | "wide" | "tall";
 
-export type TileType = "text" | "image" | "link" | "map";
-
-export type BentoTile = {
+type BaseTile = {
   id: string;
-  type: TileType;
-  size: TileSize;
   title: string;
+  size: TileSize;
 };
+
+
+export type TextTile = BaseTile & {
+  type: "text";
+  text: string;
+};
+
+export type ImageTile = BaseTile & {
+  type: "image";
+  imageUrl: string;
+  alt: string;
+};
+
+export type LinkTile = BaseTile & {
+  type: "link";
+  url: string;
+  description: string;
+};
+
+export type MapTile = BaseTile & {
+  type: "map";
+  latitude: number;
+  longitude: number;
+  label: string;
+};
+
+export type BentoTile =
+  | TextTile
+  | ImageTile
+  | LinkTile
+  | MapTile;
+
+export type TileType = BentoTile["type"];
